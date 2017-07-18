b5c2899038b678c4f15050ba68dc776c491e78cf
Chris Scott <cyberstrike@gmail.com>
2017-07-18 16:53:41 -0400

    moved jsFunction

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
-$(document).ready(function() {
-  // Tooltip only Text
-  jqueryFunction = function(){
-
-    $('.masterTooltip').hover(function(){
-      // Hover over code
-      var title = $(this).attr('title');
-      $(this).data('tipText', title).removeAttr('title');
-      $('<p class="tooltip"></p>')
-          .text(title)
-          .appendTo('body')
-          .fadeIn('slow');
-    }, function() {
-      // Hover out code
-      $(this).attr('title', $(this).data('tipText'));
-      $('.tooltip').remove();
-    }).mousemove(function(e) {
-      var mousex = e.pageX + 20; //Get X coordinates
-      var mousey = e.pageY + 10; //Get Y coordinates
-      $('.tooltip')
-          .css({ top: mousey, left: mousex })
-    });
-  }
-
-  $.get("tides/stations.json",function(stations) {window.lats=stations; });
-
+$(document).ready(function () {
+  $.get("tides/stations.json", function(stations) { window.lats = stations; });
 });
 
 function initMap() {
@@ -98,6 +74,33 @@ function initMap() {
     loadXMLDoc(passy);
 
   });
+}
+
+function jsFunction () {
+  $('.masterTooltip')
+      .hover(
+          function () {
+            // Hover over code
+            var title = $(this).attr('title');
+            $(this).data('tipText', title).removeAttr('title');
+            $('<p class="tooltip"></p>')
+                .text(title)
+                .appendTo('body')
+                .fadeIn('slow');
+          },
+          function () {
+            // Hover out code
+            $(this).attr('title', $(this).data('tipText'));
+            $('.tooltip').remove();
+          }
+      )
+      .mousemove(
+          function(e) {
+            var mouseX = e.pageX + 20; //Get X coordinates
+            var mouseY = e.pageY + 10; //Get Y coordinates
+            $('.tooltip').css({ top: mouseY, left: mouseX })
+          }
+      );
 }
 
 function getDistanceFromLatLonInKm (lat1, lon1, lat2, lon2) {
